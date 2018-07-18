55abe466605cc24cdf32eb03fc37b35be30385c5
Alex Mellor <alex.mellor@gmail.com>
2018-07-18 16:18:58 +0100

    - sort out anchor tags on course pages

---
+++ amd/src/anchor-offset.js
@@ -0,0 +1,34 @@
+define(['jquery', 'theme_woodlane/window'], function($, win) {
+
+  'use strict';
+
+    var scrollToSection = function(section) {
+        win.scrollTo(0, $(section).offset().top - 100);
+    };
+
+    var handler = function(e) {
+        e.preventDefault();
+
+        var url = $(this).attr('href');
+        var hash = url.substr(url.indexOf('#'));
+
+        if (win.history.pushState) {
+            win.history.pushState(null, null, hash);
+        } else {
+            win.location.hash = hash;
+        }
+
+        scrollToSection(hash);
+
+    };
+
+  return (function() {
+
+    if (win.location.hash) {
+        scrollToSection(win.location.hash);
+    }
+    $('#nav-drawer .is-section').on('click', handler);
+
+  })();
+
+});
